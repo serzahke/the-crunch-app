82b9a2aeebda517f9e59b7790823bc6a439196ea
serzahke <arman.kalhori@gmail.com>
2023-12-14 17:04:00 +0330

    add icons

--- src/app/components/dashboard/Menu.tsx
+++ src/app/components/dashboard/Menu.tsx
@@ -6,7 +6,7 @@ import { redirect } from 'next/navigation'
 import UserCard from '../UserCard'
 import Link from 'next/link'
 
-import { Cog8ToothIcon } from '@heroicons/react/24/solid'
+import { ChartBarIcon, ClipboardDocumentCheckIcon, Cog8ToothIcon, RssIcon, UserGroupIcon, BoltIcon } from '@heroicons/react/24/outline'
 
 const Menu = ({ userCard }: any) => {
     const { data: session } = useSession({
@@ -19,16 +19,38 @@ const Menu = ({ userCard }: any) => {
     return (
         <section className="flex flex-col gap-10 justify-between">
             <ul className="menu bg-base-200 w-56 rounded-box">
-                <li><Link href={'/dashboard/ops-benchmarks'}>Ops Benchmarks</Link></li>
-                <li><Link href={'/dashboard/feed'}>Feed</Link></li>
-                <li><Link href={'/dashboard/resource-hub'}>Resource Hub</Link></li>
-                <li><Link href={'/dashboard/automation-suite'}>Automation Suite</Link></li>
-                <li><Link href={'/dashboard/todos'}>To-Do’s</Link></li>
+                <li>
+                    <Link href={'/dashboard/ops-benchmarks'}>
+                        <ChartBarIcon className='h-4 w-4 text-black' />
+                        Ops Benchmarks
+                    </Link>
+                </li>
+                <li>
+                    <Link href={'/dashboard/feed'}>
+                        <RssIcon className='h-4 w-4 text-black' />
+                        Feed
+                    </Link>
+                </li>
+                <li>
+                    <Link href={'/dashboard/resource-hub'}>
+                        <UserGroupIcon className='h-4 w-4 text-black' />
+                        Resource Hub
+                    </Link>
+                </li>
+                <li>
+                    <Link href={'/dashboard/automation-suite'}>
+                        <BoltIcon className='h-4 w-4 text-black' />
+                        Automation Suite
+                    </Link>
+                </li>
+                <li><Link href={'/dashboard/todos'}>
+                    <ClipboardDocumentCheckIcon className='h-4 w-4 text-black' />
+                    To-Do’s</Link></li>
             </ul>
             <div className='flex flex-col gap-6'>
                 <UserCard user={session?.user} pagetype={"Client"} />
                 <Link className='btn' href={'/dashboard/account'}>
-                    <Cog8ToothIcon className='h-6 w-6 text-black'/>
+                    <Cog8ToothIcon className='h-4 w-4 text-black' />
                     Account setting</Link>
             </div>
         </section>
