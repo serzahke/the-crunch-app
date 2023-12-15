4d523616a7c0307501084f98daae407e3aacbdeb
serzahke <arman.kalhori@gmail.com>
2023-12-15 14:02:09 +0330

    navbar path set again with config git config core.ignorecase false

---
+++ src/app/components/navbar/SigninButton.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import React from 'react'
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link';
+import { ArrowLeftOnRectangleIcon, HomeIcon, RectangleGroupIcon } from '@heroicons/react/24/outline';
+
+const SigninButton = () => {
+
+    const { data: session } = useSession();
+
+    if (session && session.user) {
+        return (
+            <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn btn-ghost m-1">{session.user.name}</div>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                        <Link href={'/'}>
+                            <HomeIcon className='w-4 h-4' />
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href={'/dashboard'}>
+                            <RectangleGroupIcon className='w-4 h-4' />
+                            Dashboard
+                        </Link>
+                    </li>
+                    <button className='btn btn-ghost text-red-500 hover:bg-red-50' onClick={() => signOut()} >
+                        <ArrowLeftOnRectangleIcon className='w-4 h-4 text-red-400' />
+                        Sign out
+                    </button>
+                </ul>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <button className='btn btn-ghost' onClick={() => signIn()}>Sign in</button>
+        </div>
+    )
+}
+
+export default SigninButton
