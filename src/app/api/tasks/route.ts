1f835ee9b1c03c65a49272c2ea813b17da079606
serzahke <arman.kalhori@gmail.com>
2023-12-18 02:37:41 +0330

    chnge request with NextApiRequest

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,8 +1,10 @@
 import connectMongoDB from "@app/libs/mongodb";
 import Task from "@app/models/task";
+import { NextApiRequest } from "next";
+import { NextURL } from "next/dist/server/web/next-url";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST(request: NextApiRequest) {
     const { title, description, status, reporter, assigned, category, confirmedByOwner } = await request.json();
     await connectMongoDB();
     await Task.create({ title, description, status, reporter, assigned, category, confirmedByOwner });
@@ -16,6 +18,7 @@ export async function GET() {
 }
 
 export async function DELETE(request : Request) {
+    NextURL
     const id = request.nextUrl.searchParams.get("id");
     await connectMongoDB();
     await Task.findByIdAndDelete(id);
