51286fa8fdd2a104c58710888ef0637e06702109
serzahke <arman.kalhori@gmail.com>
2023-12-18 19:40:40 +0330

    fix server page

--- src/app/server/page.tsx
+++ src/app/server/page.tsx
@@ -3,9 +3,16 @@ import { authOptions } from "../api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth/next"
 import UserCard from "../components/UserCard"
 import { redirect } from "next/navigation"
+import { useSession } from "next-auth/react";
 
 export default async function ServerPage() {
-    const session = await getServerSession(authOptions)
+    // const session = await getServerSession(authOptions)
+    const { data: session } = useSession({
+        required: true,
+        onUnauthenticated() {
+            redirect('/api/auth/signin?callbackUrl=/client')
+        }
+    })
 
     if (!session) {
         redirect('/api/auth/signin?callbackUrl=/server')
