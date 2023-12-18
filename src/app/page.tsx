f905b35fea3e6606637fc35f6fd1b8d0e891751f
serzahke <arman.kalhori@gmail.com>
2023-12-18 19:15:32 +0330

    @app/app add to resolve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // import { options } from "./api/auth/[...nextauth]/options"
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "@app/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth/next"
 import Link from "next/link"
 import FeatureList from "./components/home/FeatureList"
