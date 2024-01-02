fc7278b4de7d68ff35a9926733a6a11db6310b4b
serzahke <arman.kalhori@gmail.com>
2024-01-02 08:44:30 +0330

    set upload thing for avatar maxFileCount: 1

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -17,7 +17,7 @@ const auth = async (req: Request) => ({ id: await getUserId()}); // Fake auth fu
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
